35457748e7ccd9e73a47372b9c96ccbf65ab817e
Rafael Cano López <148440299+WolfScarlet47@users.noreply.github.com>
2026-04-07 17:16:17 +0200

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 
 export default defineConfig({
-  site: 'https://astro-theme-vitesse.netlify.app/',
+  site: 'https://wolfscarlet47.github.io',
   server: {
     port: 1977,
   },
